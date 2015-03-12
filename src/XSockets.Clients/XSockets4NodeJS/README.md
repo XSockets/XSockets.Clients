d32bd56884918813954b5a00131ac2af31876e9b
Uffe Björklund <codeplanner@gmail.com>
2015-03-12 16:46:35 +0100

    added nodejs client

---
+++ src/XSockets.Clients/XSockets4NodeJS/README.md
@@ -0,0 +1,6 @@
+﻿# XSockets.NET4nodejs
+
+A basic client for full duplex communication between XSockets.NET and NodeJS
+
+Note: You will need a custom protocol in XSockets. Let us know if you want a pre-release to test it.
+
---
+++ src/XSockets.Clients/XSockets4NodeJS/app.js
@@ -0,0 +1,27 @@
+﻿var xsockets = require('./tcpClient.js');
+
+var c = new xsockets.TcpClient('127.0.0.1', 4502, ['animal','car']);
+c.controller('animal').on('cat', function(d) {
+    console.log('cat',d.says);
+});
+
+c.controller('car').on('bmw', function (d) {
+    console.log('bmw', d);
+});
+
+c.controller('animal').onopen = function(ci) {
+    console.log('connected controller', ci);
+    
+    c.controller('animal').send('cat', {says:'mjau'});
+}
+
+c.controller('car').onopen = function (ci) {
+    console.log('connected controller', ci);
+    
+    c.controller('car').send('bmw', 'wrroooom');//{says:'wroom'}
+}
+
+c.onconnected = function(d) {
+    console.log('connected', d);
+}
+c.open();
