43b1ca446954126963502ece6afbaf4883c7ec27
Uffe Björklund <uffe@xsockets.net>
2015-09-27 09:29:21 +0200

    Added photon client

---
+++ src/XSockets.Clients/XSockets.Photon/PhotonSocketClient.h
@@ -0,0 +1,42 @@
+#ifndef XSOCKETCLIENT_H
+#define XSOCKETCLIENT_H_
+#include <application.h>
+
+class PhotonSocketClient {
+	public:
+		typedef void (*OnMessageDelegate)(PhotonSocketClient client, String data);
+		typedef void (*OnDisconnectedDelegate)();
+		void SetClientId(String guid);
+		bool connect(char hostname[], int port = 80);
+        bool connected();
+        void disconnect();
+		void receiveData();
+		void setOnMessageDelegate(OnMessageDelegate onMessageDelegate);
+		void setOnDisconnectedDelegate(OnDisconnectedDelegate onDisconnectedDelegate);
+
+		void send(String controller, String topic, String data);
+
+		void openController(String controller);
+		void closeController(String controller);
+
+		void storageSet(String controller, String key, String value);
+		void storageGet(String controller, String key);
+		void storageRemove(String controller, String key);
+		void storageClear(String controller);
+
+		void addListener(String controller, String topic);
+		void removeListener(String controller, String topic);
+		String getValueAtIx(String data, int index);
+	private:
+				void transmit(String controller, String topic, String data);
+				void dispatch(String data);
+        void sendHandshake();
+        TCPClient _client;
+				String clientId = "";
+        OnMessageDelegate _onMessageDelegate;
+				OnDisconnectedDelegate _onDisconnectedDelegate;
+        bool readHandshake();
+        String readHandshakeLine();
+};
+
+#endif
